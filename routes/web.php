e83508c77f32d960eb634a4e623866659c5e9186
Elliot <webmax0110.dev@gmail.com>
2017-12-06 04:13:55 -0800

    Fix home page
    Error was because of $index->getHistory() function returning empty array
    
    Resolves: Homepage Issue

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,11 @@ Route::get('/indices', function() {
   $indices = App\Index::orderby('position', 'asc')->get();
   foreach ($indices as $index) {
     $index->history = $index->getHistory();
-    $index->increasing = $index->history[count($index->history) - 1]->value > $index->history[count($index->history) - 2]->value;
-    $index->decreasing = $index->history[count($index->history) - 1]->value < $index->history[count($index->history) - 2]->value;
+    $history_count = count($index->history);
+    if ($history_count >= 2) {
+	    $index->increasing = ( $index->history[$history_count - 1]->value > $index->history[$history_count - 2]->value );
+	    $index->decreasing = ( $index->history[$history_count - 1]->value < $index->history[$history_count - 2]->value );
+    }
   }
   return $indices;
 });
